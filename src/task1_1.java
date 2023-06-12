66c3872d127e1812fbb67f463fdf8df5068576ff
ct <wadim_fo@mail.ru>
2023-06-12 21:40:10 +0300

    Решение задачи №1 для семинара №1. Попытка упорядочить файлы в репозитории

--- src/task1_1.java
+++ src/task1_1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class task1 {
+public class task1_1 {
     public static void main(String[] args) {
         System.out.printf("Write your number N: ");
         Scanner num = new Scanner(System.in);
